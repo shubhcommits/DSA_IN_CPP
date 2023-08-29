d8a2dc84ff6a49cf63ca93717ebe33d1e7a645a1
Shubham <shm.code1@gmail.com>
2023-08-29 08:12:56 +0530

    Array ADT

---
+++ 3-Array/5-ArrayADT.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+class Array{
+    public:
+    int *A;
+    int size;
+    int length;
+};
+void display(Array arr){
+    cout<<"elements are :"<<endl;
+    for(int i=0;i<arr.length;i++){
+        cout<<arr.A[i]<<endl;
+    }
+}
+int main(){
+    Array arr;
+    cout<<"Enter the size of the array :"<<endl;
+    cin>>arr.size;
+    arr.A=new int[arr.size];  // this is to initialise memory in heap
+    arr.length=0;
+    int n;
+    cout<<"Enter the number of elements in array :"<<endl;
+    cin>>n;
+    cout<<"Enter the elements in the array :"<<endl;
+    for(int i=0;i<n;i++){
+        cin>>arr.A[i];
+    }
+    arr.length=n;
+    display(arr);
+
+}
