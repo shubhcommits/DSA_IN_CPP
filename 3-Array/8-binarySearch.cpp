61c0af3dcc2262a1a036fa68f6a630228d3927a8
Shubham <shm.code1@gmail.com>
2023-08-29 20:07:48 +0530

    Binary Search

---
+++ 3-Array/8-binarySearch.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Array{
+    public:
+    int A[10];
+    int size;
+    int length;
+};
+int BinarySearch(Array arr,int key){
+    int l=0;
+    int h=arr.length-1;
+    int mid;
+    while(l<=h){
+        mid=(l+h)/2;
+        if(arr.A[mid]==key){
+            return mid;
+        }
+        else if(key>mid){
+            l=mid+1;
+        }
+        else{
+            h=mid-1;
+        }
+    }
+    return -1;
+}
+int main(){
+        Array arr={{1,2,3,4,5},10,5};
+        cout<<BinarySearch(arr,2);
+}
