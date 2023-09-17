c1296c7242fb832fbc0019c695e7ce5f8c8b9938
Shubham <shm.code1@gmail.com>
2023-09-17 09:16:45 +0530

    reversing string

---
+++ 4-StringPrograms/5-reverseString.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char name[]="Welcome";
+    char B[7];
+    int i;
+    for(i=0;name[i]!='\0';i++){}
+    i=i-1;
+    int j;
+    for(j=0;i>=0;i--,j++){
+        B[j]=name[i];
+    }
+    B[j]='\0';
+    cout<<B;
+}
+/*-----------------------------SECOND_METHOD-----------------------------------------*/
+#include<iostream>
+using namespace std;
+int main(){
+    char name[]="Welcome";
+    int i,j,temp;
+    for(j=0;name[j]!='\0';j++){}
+    j=j-1;
+    for(i=0;i<j;i++,j--){
+        temp=name[i];
+        name[i]=name[j];
+        name[j]=temp;
+    }
+    cout<<name;
+}
