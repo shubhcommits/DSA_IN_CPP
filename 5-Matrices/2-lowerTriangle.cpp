d61553e45f90c702ec114f5a8f50f71b0e605068
Shubham <shm.code1@gmail.com>
2023-09-20 08:14:05 +0530

    matrices

---
+++ 5-Matrices/2-lowerTriangle.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+class Lowtri{
+    int *A;
+    int n;
+    public:
+    Lowtri(int n){
+        this->n=n;
+        // A=new int[n];
+        A=new int[n*(n+1)/2];
+    }
+    void Set(int i,int j,int x);
+    void Display();
+};
+void Lowtri::Set(int i,int j,int k){
+    if(i>=j){
+        A[i*(i-1)/2+j-1]=k;
+    }
+}
+void Lowtri::Display(){
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(i>=j)
+            cout<<A[i*(i-1)/2+j-1]<<" ";
+            else
+            cout<<"0 ";
+        }
+        cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the dimention of the array";
+    cin>>n;
+    Lowtri s(n);
+    int x;
+    cout<<"Enter the elements ";
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>x;
+            s.Set(i,j,x);
+        }
+    }
+    s.Display();
+}
+// This code is written on conserdering the matrices are starting with the index 1
---
+++ 5-Matrices/6-symmetric.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class Symmetric{
+    private:
+    int *A;
+    int n;
+    public:
+    Symmetric(int n){
+        this->n=n;
+        A=new int[n*(n+1)/2];
+    }
+    void Set(int i,int j,int x);
+    void Display();
+};
+    void Symmetric::Set(int i,int j,int x){
+        if(i<=j){
+            A[i*(i-1)/2+j-1]=x;
+        }
+    }
+    void Symmetric::Display(){
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<=n;j++){
+                if(i<=j){
+                cout<<A[i*(i-1)/2+j-1]<<" ";
+                }
+                else{
+                    int k;
+                    k=i;
+                    i=j;
+                    j=k;
+                    cout<<A[j*(j-1)/2+i-1]<<" ";
+                }
+            }
+            cout<<endl;
+        }
+    }
+int main(){
+    int n;
+    cout<<"Enter dimention of the matrix";
+    cin>>n;
+    int x;
+    Symmetric s(n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            cin>>x;
+            s.Set(i,j,x);
+        }
+    }
+    s.Display();
+}
