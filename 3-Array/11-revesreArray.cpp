bea7d818e285edb3b0e34ecd2a671d67128b3083
Shubham <shm.code1@gmail.com>
2023-09-22 23:06:04 +0530

    modified

--- 3-Array/11-revesreArray.cpp
+++ 3-Array/11-revesreArray.cpp
@@ -1,67 +1,120 @@
+// #include<iostream>
+// using namespace std;
+// class Array{
+//     public:
+//     int A[10];
+//     int size;
+//     int length;
+// };
+// // display function
+// void Display(Array arr){
+//     for(int i=0;i<arr.length;i++){
+//         cout<<arr.A[i]<<endl;
+//     }
+// }
+// // swap function
+// void swap(int *x,int *y){  // for modifying something we always have to use pointer and call by address
+//     int temp;
+//     temp=*x;
+//     *x=*y;
+//     *y=temp;
+
+// }
+// // Reversing the array
+// void rev(Array *arr){
+//     int *B;
+//     B=new int[10];
+//     int i,j;
+//     for(i=arr->length-1,j=0;i>=0;i--,j++){
+//         B[j]=arr->A[i];
+//     }
+//     for(i=0;i<arr->length;i++){
+//         arr->A[i]=B[i];
+//     }
+//     delete []B;  // deleting this to avoid memory leak
+// }
+// // this is method 2 for array reverse
+// void rev2(Array *arr){
+//     for(int i=0,j=arr->length-1;i<j;i++,j--){
+//         swap(&arr->A[i],&arr->A[j]);
+//     }
+// }
+// // left shift of array
+// void leftShift(Array *arr){
+//     for(int i=0;i<arr->length;i++){
+//         arr->A[i]=arr->A[i+1];
+//     }
+// }
+// // left rotate
+// void leftRotate(Array *arr){
+//     if(arr->length<=1){
+//         return;
+//     }
+//     int temp=arr->A[0];
+//      for(int i=0;i<arr->length;i++){
+//         arr->A[i]=arr->A[i+1];
+       
+//     }
+//     arr->A[arr->length-1]=temp;
+// }
+// int main(){
+//     Array arr = {{2,5,4,6,9},10,5};
+//     // rev(&arr);
+//     rev2(&arr);
+//     // leftShift(&arr);
+//     // leftRotate(&arr);
+//     Display(arr);
+// }
+/*------------------------------------------------------------------------------------*/
 #include<iostream>
 using namespace std;
-class Array{
+class Reverse{
+    private:
+    int *A;
+    int n;
     public:
-    int A[10];
-    int size;
-    int length;
+    Reverse(int n){
+        this->n=n;
+        A=new int[n];
+    }
+    ~Reverse(){
+        delete []A;
+    }
+    void reverse();
+    void swap(int *x,int *y);
+    void Display();
+    void Set(int i,int x);
 };
-// display function
-void Display(Array arr){
-    for(int i=0;i<arr.length;i++){
-        cout<<arr.A[i]<<endl;
+void Reverse::reverse(){
+    for(int i=0,j=n-1;i<j;i++,j--){
+        swap(&A[i],&A[j]);
     }
 }
-// swap function
-void swap(int *x,int *y){  // for modifying something we always have to use pointer and call by address
+void Reverse::swap(int *x,int *y){  // to modify something we have to pass the pointer
     int temp;
     temp=*x;
     *x=*y;
     *y=temp;
-
 }
-// Reversing the array
-void rev(Array *arr){
-    int *B;
-    B=new int[10];
-    int i,j;
-    for(i=arr->length-1,j=0;i>=0;i--,j++){
-        B[j]=arr->A[i];
-    }
-    for(i=0;i<arr->length;i++){
-        arr->A[i]=B[i];
-    }
-    delete []B;  // deleting this to avoid memory leak
-}
-// this is method 2 for array reverse
-void rev2(Array *arr){
-    for(int i=0,j=arr->length-1;i<j;i++,j--){
-        swap(&arr->A[i],&arr->A[j]);
+void Reverse::Display(){
+    for(int i=0;i<n;i++){
+        cout<<A[i]<<" ";
     }
 }
-// left shift of array
-void leftShift(Array *arr){
-    for(int i=0;i<arr->length;i++){
-        arr->A[i]=arr->A[i+1];
-    }
-}
-// left rotate
-void leftRotate(Array *arr){
-    if(arr->length<=1){
-        return;
-    }
-    int temp=arr->A[0];
-     for(int i=0;i<arr->length;i++){
-        arr->A[i]=arr->A[i+1];
-       
-    }
-    arr->A[arr->length-1]=temp;
+void Reverse::Set(int i,int x){
+    A[i]=x;
 }
 int main(){
-    Array arr = {{2,5,4,6,9},10,5};
-    // rev(&arr);
-    // rev2(&arr);
-    // leftShift(&arr);
-    leftRotate(&arr);
-    Display(arr);
+    int n;
+    cout<<"Enter the length of the array"<<endl;
+    cin>>n;
+    Reverse r(n);
+    cout<<"Enter the elements"<<endl;
+    int x;
+    for(int i=0;i<n;i++){
+        cin>>x;
+        r.Set(i,x);
+    }
+    r.reverse();
+    r.Display();
 }
--- 3-Array/15-mergeArray.cpp
+++ 3-Array/15-mergeArray.cpp
@@ -105,6 +105,90 @@ void merge(Array1 *arr1, Array1 *arr2)
 int main()
 {
     Array1 arr1{{1, 2, 3, 4, 5}, 5, 5};
-    Array1 arr2{{6, 7, 8, 9, 10}, 5, 5};
+    Array1 arr2{{6, 7, 8, 2, 10}, 5, 5};
     merge(&arr1, &arr2);
+}
+/*----------------------------------------------------------------------------------------*/
+#include<iostream>
+using namespace std;
+class Array{
+    private:
+    int *A;
+    int size;
+    int length;
+    public:
+    Array(int size){
+        this->size=size;
+        A=new int[size];
+        length=0;
+    }
+    // ~Array(){  // using destructur gives free() : double free space
+    //     delete []A;
+    // }
+    void insert(int index,int x){
+        if(index>=0&&index<=length){
+            for(int i=length-1;i>=index;i--){
+                A[i+1]=A[i];
+            }
+            A[index]=x;
+            length++;
+        }
+    }
+    void display(){
+        for(int i=0;i<length;i++){
+            cout<<A[i]<<" ";
+        }
+        cout<<endl;
+    }
+    Array* merge(Array arr2);
+};
+    Array* Array::merge(Array arr2){
+        int i,j,k;
+        i=j=k=0;
+        Array *arr3=new Array(length+arr2.length);
+        while(i<length&&j<arr2.length){
+            if(A[i]<arr2.A[j]){
+                arr3->A[k++]=A[i++];
+            }
+            else{
+                arr3[k++]=arr2.A[j++];
+            }
+        }
+        while(i<length){
+            arr3->A[k++]=A[i++];
+        }
+        while(j<arr2.length){
+            arr3->A[k++]=arr2.A[j++];
+        }
+        arr3->length = length + arr2.length;
+        return arr3;
+    }
+int main(){
+    int size1;
+    int index;
+    cout<<"Enter the size of the first array : ";
+    cin>>size1;
+    Array arr1(size1);
+    int x;
+    cout<<"Enter index and element you want to enter in arr1 : ";
+    for(int i=0;i<size1;i++){
+        cin>>index;
+        cin>>x;
+        arr1.insert(index,x);
+    }
+    int size2;
+    cout<<"Enter the size of thr array 2 : ";
+    cin>>size2;
+    Array arr2(size2);
+    cout<<"Enter index and element you want to enter in arr1 : ";
+    for(int i=0;i<size2;i++){
+        cin>>index;
+        cin>>x;
+        arr2.insert(index,x);
+    }
+    // Array* arr3=nullptr;
+    Array* arr3;
+    arr3=arr1.merge(arr2);
+    arr3->display();
+    return 0;
 }
