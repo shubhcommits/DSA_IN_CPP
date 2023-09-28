46328daa516a6a86681d01ae44ba076dcc1e6028
Shubham <shm.code1@gmail.com>
2023-09-28 19:31:41 +0530

    corrected memory leaking bugs

--- 3-Array/6-someFunct.cpp
+++ 3-Array/6-someFunct.cpp
@@ -1,73 +1,82 @@
-// This contain display(),add(),insert(),delete() function
-#include <iostream>
+// contains insert(),delete(),add(),display()
+#include<iostream>
 using namespace std;
-class Array
-{
-public:
-    int A[10];
+class Array{
+    private:
+    int *A;
     int size;
     int length;
+    public:
+    Array(int sz){
+        size=sz;
+        length=0;
+        A=new int[size];
+    }
+    ~Array(){
+        delete []A;
+    }
+    void Display();
+    void InsertElements(int n);
+    void Add(int x);
+    void Insert(int index,int x);
+    void Delete(int index);
 };
-// display function
-void Display(Array arr)
-{
-    for (int i = 0; i < arr.length; i++)
-    {
-        cout << arr.A[i] << endl;
+void Array::InsertElements(int n){
+    if(n<=size){
+    for(int i=0;i<n;i++){
+        cin>>A[i];
+        length=n;
+    }
+    }
+    else{
+        cout<<"Not enough space to insert"<<endl;
     }
 }
-// add function
-void add(Array *arr, int x)
-{ // when we have edit something then we send pointer to the array
-    if (arr->length < arr->size)
-    {
-        arr->A[arr->length++] = x;
+void Array::Display(){
+    cout<<"Elements are :"<<endl;   
+    for(int i=0;i<length;i++){
+        cout<<A[i]<<endl;
     }
 }
-// insert function
-void insert(Array *arr, int index, int x)
-{ // this will insert the elemnet at index we want
-    if (index >= 0 && index <= arr->length)
-    {
-        for (int i = arr->length; i > index; i--)
-        {
-            arr->A[i] = arr->A[i - 1];
+void Array::Add(int x){
+    if(length<size){
+        A[length++]=x;
+    }
+    else{
+        cout<<"Array is full"<<endl;
+    }
+}
+void Array::Insert(int index,int x){
+    if(index>=0&&index<=length&&length<size){
+        for(int i=length;i>index;i--){
+            A[i]=A[i-1];
         }
-        arr->A[index] = x;
-        arr->length++;
+        A[index]=x;
+        length++;
+    }
+    else{
+        cout<<"Invalid index or array is full" << endl;
     }
 }
-// delete function
-void Delete(Array *arr, int index)
-{
-    if (index >= 0 && index < arr->length)
-    {
-        for (int i = index; i < arr->length - 1; i++)
-        {
-            arr->A[i] = arr->A[i + 1];
+void Array::Delete(int index){
+    if(index>=0&&index<length){
+        for(int i=index;i<length-1;i++){
+            A[i]=A[i+1];
         }
-        arr->length--;
+    length--;
     }
 }
-// int Delete(Array *arr,int index){
-//     int x=0;
-//     if(index>=0 && index<arr->length){
-//         x=arr->A[index];
-//         for(int i=index;i<arr->length-1;i++){
-//             arr->A[i]=arr->A[i+1];
-//         }
-//         arr->length--;
-//         return x;
-//     }
-//     return 0;
-// }
-int main()
-{
-    Array arr{{1, 5, 6, 8, 4}, 10, 5}; // {{elements},size,length}
-    // add(&arr, 10);
-    // insert(&arr, 2, 10);
-    // Display(arr);
-    Delete(&arr, 0);
-    Display(arr);
-    return 0;
+int main(){
+    int sz;
+    cout<<"Enter the size of the array "<<endl;
+    cin>>sz;
+    Array arr(sz);
+    int n;
+    cout<<"Enter the number of elements in array"<<endl;
+    cin>>n;
+    arr.InsertElements(n);
+    // arr.Add(15);
+    arr.Insert(5,16);
+    arr.Delete(2);
+    arr.Display();
 }
